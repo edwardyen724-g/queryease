fcfed9a5687845af0de56013c0c0b73673f5b9ee
edwardyen724-g <65644998+edwardyen724-g@users.noreply.github.com>
2026-03-06 05:21:52 -0800

    feat: add missing module src/lib/mongoose.ts

---
+++ src/lib/mongoose.ts
@@ -0,0 +1,6 @@
+import mongoose from 'mongoose';
+
+export const dbConnect = async () => {
+  if (mongoose.connection.readyState >= 1) return;
+  await mongoose.connect(process.env.MONGODB_URI || '');
+};
